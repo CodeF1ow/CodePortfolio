2a5a4def499366d9589315bdc2d9b534f39323bd
CodeFlow / <k1ri86mail@gmail.com>
2024-05-27 00:08:25 +0200

    Fixed cv

--- src/components/Resumen/ResumenNuevo.jsx
+++ src/components/Resumen/ResumenNuevo.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/codeflow.pdf";
+import pdf from "../../Assets/../Assets/oscar_cv.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -31,7 +31,7 @@ function ResumeNew() {
           </Button>
         </Row>
 
-        <Row className="resume">
+        <Row className="resume" style={{ justifyContent: "center" }}>
           <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
